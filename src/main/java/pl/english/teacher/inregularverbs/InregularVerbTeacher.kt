efc330bf1b02784f5546fbe721d7490ec08b9da3
gszymczuch <grzegorz.szymczuch@britenet.com.pl>
2018-12-26 14:49:47 +0100

    Add todo for inregular verbs teacher

--- src/main/java/pl/english/teacher/inregularverbs/InregularVerbTeacher.kt
+++ src/main/java/pl/english/teacher/inregularverbs/InregularVerbTeacher.kt
@@ -4,6 +4,17 @@ import java.lang.System.currentTimeMillis
 import java.nio.file.Paths
 import java.util.*
 
+//TODO
+// 1. dodac obsluge poprawnie wprowadzonych slowek i zle (countery w pliku ze slowkami)
+// 2. czyszczenie konsoli po wprowadzeniu przez uzytkownika danych. Do kotlin console dodac
+// metode clear
+// 3. dodac test do nauki czasow czyli zdanie w present simple i jak zamienic to zdanie na
+// present perfect. Template w present perfect i przygotowany template w present perfect
+// 4. wyswietlanie na koniec statystyk ile slowek udalo sie poprawnie nauczyc + slowka ze zlymi
+// odpowiedziami
+// 5. mozliwosc nauki slowek, ktore sprawiaja najwiecej problemow
+// 6. tryb zaawansowany gdzie kolejnosc form (I, II, II) sa w innej kolejnosci na ekranie
+// 7. dodanie sprawdzania wprowadzonych slowek z ignore_case
 fun main() {
     val inregularVerbs = InregularVerbParser().parseFile(Paths.get("inregularVerbs.csv"))
 
