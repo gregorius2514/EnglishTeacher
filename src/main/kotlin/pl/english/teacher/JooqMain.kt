efa6b385fb09566f562f3b0c86e83f3519aced1a
grzesiek <gregorius2514@gmail.com>
2019-07-15 21:01:39 +0200

    change postgresql driver dependence

--- src/main/kotlin/pl/english/teacher/JooqMain.kt
+++ src/main/kotlin/pl/english/teacher/JooqMain.kt
@@ -1,14 +1,30 @@
 package pl.english.teacher
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
-import java.sql.DriverManager
+import javax.sql.DataSource
 
 // run docker image
 // docker run --name postgres -d -p 5432:5432 -e POSTGRES_PASSWORD=root postgres:11.4
 fun main() {
-    val con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "root")
-    val dslContext = DSL.using(con, SQLDialect.POSTGRES);
 
-    val fruits = dslContext.fetch("Select * from account")
+    val hikariConfig = HikariConfig()
+    hikariConfig.jdbcUrl = "jdbc:postgresql://localhost:5432/english_teacher"
+    hikariConfig.username = "english_teacher"
+    hikariConfig.password = "english_teacher"
+    hikariConfig.connectionTestQuery = "SELECT 1 "
+    
+    val hikariDataSource: DataSource = HikariDataSource(hikariConfig)
+
+     DSL.using(hikariDataSource, SQLDialect.POSTGRES).let {dslContext ->
+         dslContext.fetch("select * from account")
+        
+    }
+//    val fruits = dslContext.fetch("Select * from account")
+
+//    dslContext.insertInto(ACCOUNT, )
+   
+//    dslContext.close()
 }
