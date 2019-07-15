3a9c2a1773fc19d2555accff7674c7e5ddde9ea2
grzesiek <gregorius2514@gmail.com>
2019-07-15 21:22:01 +0200

    Project little refactoring

--- src/main/kotlin/pl/english/teacher/JooqMain.kt
+++ src/main/kotlin/pl/english/teacher/JooqMain.kt
@@ -9,22 +9,16 @@ import javax.sql.DataSource
 // run docker image
 // docker run --name postgres -d -p 5432:5432 -e POSTGRES_PASSWORD=root postgres:11.4
 fun main() {
-
     val hikariConfig = HikariConfig()
     hikariConfig.jdbcUrl = "jdbc:postgresql://localhost:5432/english_teacher"
     hikariConfig.username = "english_teacher"
     hikariConfig.password = "english_teacher"
     hikariConfig.connectionTestQuery = "SELECT 1 "
-    
+
     val hikariDataSource: DataSource = HikariDataSource(hikariConfig)
 
-     DSL.using(hikariDataSource, SQLDialect.POSTGRES).let {dslContext ->
-         dslContext.fetch("select * from account")
-        
-    }
-//    val fruits = dslContext.fetch("Select * from account")
+    DSL.using(hikariDataSource, SQLDialect.POSTGRES).let { dslContext ->
+        dslContext.fetch("select * from account")
 
-//    dslContext.insertInto(ACCOUNT, )
-   
-//    dslContext.close()
+    }
 }
