54f6e7bfb5c675a7a15cc0285394477d5fb45a9c
grzesiek <gregorius2514@gmail.com>
2019-07-17 21:24:14 +0200

    Fixed jooq generation code

--- src/main/kotlin/pl/english/teacher/JooqMain.kt
+++ src/main/kotlin/pl/english/teacher/JooqMain.kt
@@ -4,7 +4,10 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
+import pl.english.teacher.generated.tables.Account
+import java.sql.Timestamp
 import javax.sql.DataSource
+import pl.english.teacher.generated.tables.Account.ACCOUNT as ACCOUNT
 
 // run docker image
 // docker run --name postgres -d -p 5432:5432 -e POSTGRES_PASSWORD=root postgres:11.4
@@ -19,6 +22,11 @@ fun main() {
 
     DSL.using(hikariDataSource, SQLDialect.POSTGRES).let { dslContext ->
         dslContext.fetch("select * from account")
+        
+        dslContext.insertInto(ACCOUNT, ACCOUNT.USER_ID, ACCOUNT.USERNAME, ACCOUNT.PASSWORD, ACCOUNT.EMAIL, ACCOUNT.CREATED_ON)
+                .values(100, "admin", "admin", "admin@o2.pl", Timestamp(System.currentTimeMillis()))
+                .execute()
 
+        dslContext.fetch("select * from account")
     }
 }
